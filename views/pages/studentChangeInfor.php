5b6fb55f9ea2d1e1ef5f6ac8842eb4eee0c0c7dc
Tranggg1210 <nguyenthitrang.ttd@gmail.com>
2024-06-22 18:48:35 +0700

    ✨ complete student change infor

--- views/pages/studentChangeInfor.php
+++ views/pages/studentChangeInfor.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Change User Information</title>
     <style>
@@ -25,6 +26,20 @@
         .btn-primary {
             margin-left: 12px;
         }
+        .gender{
+            display: flex;
+            justify-content: start;
+        }
+        .gender > div{
+            display: flex;
+            align-items: center;
+        }
+        .gender > div > input{
+            margin: 0 8px;
+        }
+        .gender > div:nth-child(2){
+            margin: 0 16px;
+        }
     </style>
 </head>
 
@@ -40,32 +55,45 @@
             include __DIR__ . '/../components/header.php';
             ?>
             <div class="container-body">
-                <div class="form" id="checkPassword" >
+                <div class="form" id="checkPassword">
                     <h2 class="h2">Thông tin cá nhân</h2>
                     <form method="POST">
                         <div class="mb-3">
                             <label for="code" class="col-form-label">Mã sinh viên:<span class="span">*</span></label>
-                            <input disabled type="text" class="form-control" name="code" value="<?php echo $username; ?>">
+                            <input disabled type="text" class="form-control" name="code" value="<?php echo $code; ?>">
                         </div>
                         <div class="mb-3">
                             <label for="fullName" class="col-form-label">Họ và tên:<span class="span">*</span></label>
-                            <input required type="text" class="form-control" name="fullName" value="<?php echo $username; ?>">
+                            <input required type="text" class="form-control" name="fullName" value="<?php echo $fullName; ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label for="gender" class="col-form-label">Giới tính:<span class="span">*</span></label> <br>
+                            <div class="gender">
+                                <div>
+                                    <input required type="radio" id="gender_male" name="gender" value="Nam" <?php echo ($gender == 'Nam') ? 'checked' : ''; ?>>
+                                    <label for="gender_male">Nam</label>
+                                </div>
+                                <div>
+                                    <input required type="radio" id="gender_female" name="gender" value="Nữ" <?php echo ($gender == 'Nữ') ? 'checked' : ''; ?>>
+                                    <label for="gender_female">Nữ</label>
+                                </div>
+                            </div>
                         </div>
                         <div class="mb-3">
                             <label for="birthDate" class="col-form-label">Ngày sinh:<span class="span">*</span></label>
-                            <input required type="text" class="form-control" name="birthDate" value="<?php echo $username; ?>">
+                            <input required type="date" class="form-control" name="birthDate" value="<?php echo $formattedDate; ?>">
                         </div>
                         <div class="mb-3">
                             <label for="phoneNumber" class="col-form-label">Số điện thoại:<span class="span">*</span></label>
-                            <input required type="text" class="form-control" name="phoneNumber" value="<?php echo $username; ?>">
+                            <input required type="text" class="form-control" name="phoneNumber" value="<?php echo $phoneNumber; ?>">
                         </div>
                         <div class="mb-3">
                             <label for="email" class="col-form-label">Email:<span class="span">*</span></label>
-                            <input required type="text" class="form-control" name="email" value="<?php echo $username; ?>">
+                            <input required type="text" class="form-control" name="email" value="<?php echo $email; ?>">
                         </div>
                         <br>
                         <div class="modal-footer">
-                            <button type="submit" class="btn btn-primary" name="nextToCheckPassword">Thay đổi</button>
+                            <button type="submit" class="btn btn-primary" name="changeInfor">Cập nhập thông tin</button>
                         </div>
                     </form>
                 </div>
